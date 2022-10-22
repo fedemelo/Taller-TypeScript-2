d94b7cca9a5e0acfdaad135d2b45c6b1be9012dc
Federico Melo Barrero <f.melo@uniandes.edu.co>
2022-10-21 21:00:05 -0500

    Cambios cupi

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { Serie } from './serie.js';
 import { series } from './data.js';
 
 function insertSeries(series: Serie[]): void {
-    let tbody: HTMLElement = document.getElementById('series')!;
+    const tbody: HTMLElement = document.getElementById('series')!;
     console.log('Desplegando series.');
     series.map(s => {
         let tr: HTMLElement = document.createElement('tr');
@@ -21,9 +21,31 @@ function insertSeasonsAvrg(series: Serie[]): void {
     series.map(s => {sum += s.seasons});
     const avrg: number = sum / series.length;
     console.log('Desplegando promedio.')
-    let table: HTMLElement = document.getElementById('tabla-series')!;
+    const table: HTMLElement = document.getElementById('tabla-series')!;
     const html: string = '<p>Seasons average: '+avrg+'</p>'
     table.insertAdjacentHTML('afterend', html)
+}
+
+function showDetail(series: Serie[]): void {
+    const tbody: HTMLElement = document.getElementById('series')!;
+    const trs: HTMLCollection = tbody.getElementsByTagName('tr')!;
+    const col: HTMLElement = document.getElementById('detail')!;
+
+    for (let i: number = 0; i < trs.length; i++) {
+        let tr: HTMLElement = trs[i];
+        tr.addEventListener('click', function() {
+            console.log('Recibe click')
+            const html: string = `
+            <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src=${series[i].cover} alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">${series[i].name}</h5>
+              <p class="card-text">${series[i].description}</p>
+            </div>
+          </div>`
+            col.innerHTML = html;
+        });
+    }
 }
 
 insertSeries(series);
